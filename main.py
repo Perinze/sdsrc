2e815deb11a7b6092832f04f9125979ef692d145
perinze <perinzefaper@foxmail.com>
2022-04-12 22:50:53 +0800

    non-blocking

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from multiprocessing.sharedctypes import Value
 import monitor
 import qserver
 from sys import argv, stderr
+from queue import Empty
 
 if __name__ == '__main__':
 	print(argv)
@@ -12,14 +13,18 @@ if __name__ == '__main__':
 	port = int(port)
 	qs = qserver.QueueServer(host, port)
 	
-	mon = monitor.Monitor([[1, 1], [4, 6]], [0, 8], [0, 8], 16)
+	mon = monitor.Monitor([[4, 4]], [0, 8], [0, 8], 16)
 
 	mon.draw()
+
+	loop_idx = 0
 	while True:
 		try:
 			data = qs.pop()
-			print(data)
-			mon.plot(data)
+			if len(data) != 0:
+				print(data)
+				mon.plot(data)
+				print(f"log::loop index {loop_idx}")
 			mon.update()
 		except KeyboardInterrupt:
 			exit(0)
@@ -27,4 +32,8 @@ if __name__ == '__main__':
 			pass
 		except ValueError:
 			pass
+		except Empty:
+			pass
+
+		loop_idx = (loop_idx + 1) % 114514
 	
